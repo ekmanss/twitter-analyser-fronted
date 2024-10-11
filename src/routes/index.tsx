6ae210a0894154803289badfb1434099e9d6d9a1
ekmanss <hfers@msn.com>
2024-10-11 20:14:08 +0800

    biome check

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
-import * as React from "react";
 import { Button } from "@/components/ui/button";
+import * as React from "react";
 import { Link } from "react-router-dom";
 
 export default function IndexPage() {
